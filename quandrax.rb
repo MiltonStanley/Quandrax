549e7bba7cd268465a676dfdab34efb7e108f44e
MiltonStanley <milton.stanley@gmail.com>
2012-09-28 08:28:24 -0500

    Fixed tagify, cleaned up flipflop, added a tester file for conversions

--- quandrax.rb
+++ quandrax.rb
@@ -1,3 +1,13 @@
+###################################################################
+#
+# Title: Quandrax
+# Description:  The QUick ANd Dirty paRAdoX converter, a project to create a single, unified save game converter.
+# Version: 0.1
+# (c) 2012, Milton Stanley
+#
+###################################################################
+
+
 {
 =begin
     -- Tag Map
@@ -48,7 +58,7 @@ require './tag_map.rb'
 
 #### CONSTANTS ####
 #$oldFile = File.open("short_form.rb",'r')
-$oldFile = File.open("WilliamBeginning.ck2",'r')
+$oldFile = File.open("./workflow/WilliamBeginning.ck2",'r')
 
 $goodTitle =  /^[bcdke]_\w+/	# RegEx for title data, meaning "beginning of string (^) is either a b,c,d,k,or e, and is then followed by an _,
             # and then one or more (+) word characters (\w)
@@ -60,16 +70,7 @@ $rulerFromFile = Array.new	# Ruler is blank array; will populate as array[index]
 $rulerFromFile << "NO_PROVINCE"	# Array is 0 indexed; provinces are 1, so push a blank string to start, so appending will line up
 $liegeFromFile = Hash.new
   
-$province_mapping = [370, 371, 372, 372, 372]
-#~ $tag_map = {
-  #~ "d_one" => "IRE",
-  #~ "c_two" => "ENG",
-  #~ "c_three" => "SPA",
-  #~ "k_four" => "FRA",
-  #~ "e_five" => "POR",
-  #~ "d_normandy" => "ENG"
-  #~ }
-  
+$province_mapping = [0,370, 371, 372, 372, 372]
 
 #### CLASSES ####
 
@@ -81,9 +82,9 @@ class Player
   end	
 
   def tagify
-    @who = $tag_map[@who.intern]		
+    @who = $tag_map[@who]		
   end
-
+  
 end
 
 class Province	# Province class - has an ID (EU3 location) and a controller (shifts in runtime - CK2 vassal - CK2 liege - EU3 tag)
@@ -96,42 +97,50 @@ class Province	# Province class - has an ID (EU3 location) and a controller (shi
   end
 
   def annex(newController)	# How we change controller at runtime
-    @controller = newController   
+    @controller = newController unless @controller == $player.who  
   end
 
 end
 
 class World < Array	# World class - is an Array made up of province objects
   
+  attr_reader :game
+  
+  def initialize(game)
+    @game = game
+  end
+  
   def flipflop
-    new_world = World.new
-    self.each_index do |index|
-      prov = self[index]
-      if prov.id.class == Array
-        #puts prov.id
-        prov.id.each_index do |ck2|
-          if new_world[prov.id[ck2]].nil?
-            new_world[prov.id[ck2]] = Province.new(self.index(prov), prov.controller)
+    new_world = World.new('eu3') if self.game == 'ck2'
+    new_world = World.new('ck2') if self.game == 'eu3'
+    self.each_index do |index|      # Iterate over each index (index = @game index)
+      prov = self[index]                # prov = province at that index
+      next if prov.nil? 
+      new_province = prov.id                  # new_province = the OTHER game's id
+      # Does the current province become multiple provinces?
+      
+      if new_province.class == Array      # -> Yes, it does
+        new_province.each_index do |id|  # Cycle through the new province id's
+          if new_world[new_province[id]].nil?     # Make sure the new province doesn't exist yet
+            new_world[new_province[id]] = Province.new(self.index(prov), prov.controller)
           else
+            # Will pull current ids, put in array, add new one, then put them all back in id
             puts 'Already there!'
           end
         end
-      else
-        if new_world[prov.id].nil?
-          new_world[prov.id] = Province.new(index, prov.controller)
-        elsif new_world[prov.id].id.class == Fixnum
-          _temp = new_world[prov.id].id
-          new_world[prov.id].id = Array.new
-          new_world[prov.id].id << _temp
-          new_world[prov.id].id << index
-          #new_world[prov.id] = Province.new(index, "ERROR ERROR")
-        elsif new_world[prov.id].id.class == Array
-          _temp = new_world[prov.id].id
-          new_world[prov.id].id = Array.new
-          _temp.each { |id| new_world[prov.id].id << id }
-          new_world[prov.id].id << index
+     
+     else  # -> no, there is 1:1 province matching
+        if new_world[new_province].nil?   # The new province doesn't exist yet
+          new_world[new_province] = Province.new(index, prov.controller)
+        elsif new_world[new_province].id.class == Fixnum  # The new province exists, but only has 1 id
+          _temp = new_world[new_province].id
+          new_world[new_province].id = Array.new
+          new_world[new_province].id << _temp << index
+        elsif new_world[new_province].id.class == Array   # The new province exists, and has multiple id's
+          new_world[new_province].id << index
         end
       end
+    
     end
     new_world
   end
@@ -143,8 +152,9 @@ class World < Array	# World class - is an Array made up of province objects
   end
   
   def debug		# For testing - lets us look at Province objects inside in a clean way
+    puts "Game Type: #{@game}"
     self.each do |province|
-      puts "ID:#{province.id} -- Controller:#{province.controller}" unless province.nil?
+      puts "#{self.index(province)} - ID:#{province.id} -- Controller:#{province.controller}" unless province.nil?
     end
     puts "------------------------------------------------------------"
   end
@@ -161,7 +171,7 @@ class World < Array	# World class - is an Array made up of province objects
   
   def tagify	# And finally, we convert the (hopefully) top level names to EU3 tags
     self.each do |province|
-      province.annex($tag_map[province.controller.intern]) unless $tag_map[province.controller.intern].nil? # all of them, so no if statement
+      province.annex($tag_map[province.controller]) unless $tag_map[province.controller].nil? # all of them, so no if statement
     end
   end
       
@@ -265,7 +275,7 @@ class String			# Add some helpful things to help program understand what a line 
 end
       
 #### PROGRAM ####
-map = World.new	# Create World
+map = World.new('ck2')	# Create World
 $player = Player.new
 
 
@@ -294,3 +304,7 @@ map.debug
 
 eu3 = map.flipflop
 eu3.debug
+
+ck2 = eu3.flipflop
+ck2.debug
+
