bb043824a07117f9c8b3a3f0882c2557f8f45365
MiltonStanley <milton.stanley@gmail.com>
2012-11-10 18:52:30 -0600

    got convertToTags done in scaffold

--- hashconv.cpp
+++ hashconv.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+
 using namespace std;
+
+void convertToTags(vector<string> &, map<string, string>);
 
 int main(){
   vector<string> titles;
-  titles.push_back("one");
-  titles.push_back("two");
-  titles.push_back("three");
+  titles.push_back("d_normandy");
+  titles.push_back("k_england");
+  titles.push_back("e_scandinavia");
 
-  string x;
-  cin >> x;
+  map<string, string> tagMap;
+
+  tagMap["d_normandy"] = "NRM";
+  tagMap["k_england"] = "ENG";
+  tagMap["e_scandinavia"] = "SCA";
+
+  for(int i=0;i<titles.size();i++) 
+    cout << "Title " << i << ": " << titles[i] << endl;
+
+  convertToTags(titles, tagMap);
+
+
+  for(int i=0;i<titles.size();i++) 
+    cout << "Title " << i << ": " << titles[i] << endl;
+
   return 0;
+}
+
+void convertToTags(vector<string> & titles, map<string,string> tagMap){
+  cout << "Converting to tags..." << endl;
+  for(int i=0; i<titles.size(); i++){
+    titles[i] = tagMap[titles[i]];
+  }
 }
