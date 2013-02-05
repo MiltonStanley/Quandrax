9e0576c1db659640bf8759800a5df3169b493549
MiltonStanley <milton.stanley@gmail.com>
2013-02-05 13:48:04 -0600

    Updated TODO, version numbers, removed C++ README

--- Development/C++/quandrax.cpp
+++ Development/C++/quandrax.cpp
@@ -6,7 +6,7 @@
  * Description:  The QUick ANd Dirty paRAdoX converter, a project to create 
  *               a single, unified save game converter.
  * Author:       Milton Stanley
- * Version:      1.1.0 - C++
+ * Version:      2.0.0 - C++
  * License:      Too complicated. You can use this all you want. To edit it,
  *               you must do so through the Github page via pull requests, etc.
  *               at http://www.github.com/MiltonStanley/Quandrax_Cpp
