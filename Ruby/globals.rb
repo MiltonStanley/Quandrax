34aad98324f35396104d7660799d29f05422170a
MiltonStanley <milton.stanley@gmail.com>
2013-02-25 22:50:31 -0600

    Didn't commit for some reason

---
+++ Ruby/globals.rb
@@ -0,0 +1,17 @@
+# true uses template files, false doesn't
+
+$HEADER = true
+$REBELS = true
+$IMPERIAL_INFO = true
+$RELIGIOUS_INFO = true
+$PAPACY = true
+$TRADE = true
+$PROVINCE = true
+$NATIONS = true
+$DIPLOMACY = true
+$COMBAT = true
+$ACTIVE_WAR = true
+$PREVIOUS_WAR = true
+$FOOTER_A = true
+$JAPANESE = true
+$FOOTER_B = true
