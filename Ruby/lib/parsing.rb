55315434b1103d28f2c879b7a890e204e2fc6d95
MiltonStanley <milton.stanley@gmail.com>
2013-03-01 14:18:56 -0600

    File now parses correctly all specific/generic classes

--- Ruby/lib/parsing.rb
+++ Ruby/lib/parsing.rb
@@ -5,7 +5,6 @@ class Tracker
     @line_number = line_number
     @nest_level = nest_level
     @location = 'header'
-    @generic_class = true
   end
 
   def update_nesting(line)
@@ -19,19 +18,44 @@ class Tracker
   def update(line)
     @line_number += 1 
     update_nesting(line)
-    @generic_class = make_generic_class?(line)
-    if change_location? && !close_bracket?(line) # I don't like this - not clear enough
-      @location = line.sub(/=$/,'')
+    @location = change_location?(line)
+  end
+
+  def change_location?(line)
+    if specific_class_trigger?(line)
+      return line.sub(/=$/,'')
+    elsif generic_class_trigger?(line)
+      return get_generic_class(line)
+    else
+      return @location
     end
   end
 
-  def make_generic_class?(line)
-    triggers = ['dynasties=',]
-    !(@nest_level == 0 && line == 'dynasties=')
+  def specific_class_trigger?(line)
+    @nest_level == 0 && ( line == 'dynasties=' || line == 'character=' || 
+                          line == 'id=' || line == 'diplomacy=' || line == 'combat=' ||
+                          line == 'war=' || line == 'active_war=')
   end
 
-  def change_location?  # Here's the culprit - need to simplify this and above/below functions
-    @generic_class && @nest_level == 0
+  def generic_class_trigger?(line)
+    @nest_level == 0 && (line == 'catholic=' || line == '1=' || 
+                         line == 'e_rebels=' || footer_trigger?(line) )
+  end
+
+  def get_generic_class(line)
+    if line == 'catholic='
+      return 'religious_authority'
+    elsif line == '1='
+      return 'provinces'
+    elsif line == 'e_rebels='
+      return 'titles'
+    elsif footer_trigger?(line)
+       return 'footer'
+    end
+  end
+
+  def footer_trigger?(line)
+    @location == 'active_war' && line != 'active_war=' && !close_bracket?(line)
   end
 
   def close_bracket?(line)
