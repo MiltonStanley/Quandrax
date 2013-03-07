e35fe9e9ba1a7e5dcedb5e375dfd76c0e0a22f73
MiltonStanley <milton.stanley@gmail.com>
2013-03-07 16:19:07 -0600

    Parsed character realm -> tag (automatic conversion)

--- Ruby/lib/classes/header.rb
+++ Ruby/lib/classes/header.rb
@@ -13,13 +13,36 @@ class Header
   def add(line)
     key, value = split_key_value(line)
     @date = value if is_date?(key)
-    #@tag = $TM_CK2_EU3[value] if is_realm?(line)
-    puts line if is_realm?(line)
+    @tag = $TM_CK2_EU3[value.gsub("\"",'')] if is_realm?(key)
   end
 
   def write
     puts "date=#{@date}"
     puts "player=#{@tag}"
+    
+=begin
+    monarch=6840    // http://www.paradoxian.org/eu3wiki/Common%5Ccountries_folder
+    cardinal=16     // "
+    leader=1184     // "
+    advisor=1889    // "
+    rebel=41        // Not sure what this is
+    unit=1589       // Probably related
+    flags=          // STAYS
+    {               // THE
+    }               // SAME
+    gameplaysettings=                 // fiddle with settings to figure this out  
+    {                                 # Just realized I used C++ comments...
+      setgameplayoptions=             #
+      {                               #
+    0 0 0 0 0 0 0 2 0 1 0 0 0   }     #
+    }                                 //  
+    start_date="1399.10.14"           // same as CK2 *unless it breaks*
+    id=                               // Gotta figure this out
+    {                                 #
+      id=3001                         #
+      type=4713                       #
+    }                                 #
+=end
   end
 
   def is_date?(key)
