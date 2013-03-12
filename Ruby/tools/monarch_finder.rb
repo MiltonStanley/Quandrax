c51d9dc79fe022286f88576691649db5e2590c00
MiltonStanley <milton.stanley@gmail.com>
2013-03-11 21:00:55 -0500

    Working on parser

--- Ruby/tools/monarch_finder.rb
+++ Ruby/tools/monarch_finder.rb
@@ -4,17 +4,47 @@ def nation_header?(line)
   !(line =~/^[\s\t]/) && !(line =~ /^\d/) && line.length == 4
 end
 
-new_nation = true
-new_monarch = true
+def monarch_line?(line)
+  line =~ /^\s+(monarch=)/
+end
+
+def id_line?(line)
+  line =~ /^\s+(id=)\d+/
+end
+
+new_nation = false
+new_monarch = false
 
 while line = file.gets
   line.chomp!
   if nation_header?(line)
+    nation = line.gsub!('=','')
     new_nation = true
-    nation = line.splice!
   end
-  if new_nation
-    puts line if line =~ /^\s+(monarch=)/
-  end  
+  if monarch_line?(line) && new_nation
+    new_monarch = true
+  end
+  if id_line?(line) && new_nation && new_monarch
+    puts "#{nation}: #{line}"
+    new_nation = false
+    new_monarch = false
+  end
 	
 end
+
+=begin
+found a new nation?
+  store it
+  turn off new nation
+  wait for monarch...
+found a monarch?
+  turn off new monarch
+  wait for actual id...
+found an id?
+  store it!
+  turn on monarch and nation
+end
+
+
+
+=end
