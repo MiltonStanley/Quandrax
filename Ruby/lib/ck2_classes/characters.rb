4a2b33a57e4d834540e379ec70621b9672911f5e
MiltonStanley <milton.stanley@gmail.com>
2013-03-23 21:16:41 -0500

    Starts & stops w/ pope char info

--- Ruby/lib/ck2_classes/characters.rb
+++ Ruby/lib/ck2_classes/characters.rb
@@ -2,14 +2,21 @@ class CK2_Characters
   
   def initialize
     puts "Reading CK2 character section..."
+    @papal_relations = Hash.new
+    @in_pope_char_info = false
   end
 
   def add(line)
-  	puts line if starting_pope_char_info?(line)
+  	@in_pope_char_info = true if starting_pope_char_info?(line)
+    @in_pope_char_info = false if new_char_info?(line) && @in_pope_char_info
   end
 
   def starting_pope_char_info?(line)
   	line =~ /^\t#{$POPE}=/
+  end
+
+  def new_char_info?(line)
+    line =~ /^\t\d+=/ && !(line =~ /^\t#{$POPE}=/)
   end
 
 end
