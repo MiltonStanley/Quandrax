65b9ea6e18fff186fb0729fc3f8ba9c0230a9dd3
MiltonStanley <milton.stanley@gmail.com>
2013-03-26 13:49:35 -0500

    Working on selecting cardinals

--- Ruby/lib/eu3_classes/papal_information.rb
+++ Ruby/lib/eu3_classes/papal_information.rb
@@ -1,7 +1,13 @@
 class EU3_Papal_Information
 
-  def initialize(titles, papal_relations)
+  def initialize(titles, papal_relations) # Title info, and allies of pope hash
     puts "Creating EU3 papacy section"
+    @cardinals = Hash.new
+    create_cardinals(papal_relations)
+  end
+
+  def create_cardinals(papal_allies)
+    papal_allies = papal_allies.sort { |a, b| a[1]<=>b[1] } # Converts to arary
   end
 
 end
