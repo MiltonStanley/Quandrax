7fc3131d0ecf363df72b7b82ad9c3732c7298539
MiltonStanley <milton.stanley@gmail.com>
2013-03-26 18:53:41 -0500

    @cardinals holds eligible folks

--- Ruby/lib/eu3_classes/papal_information.rb
+++ Ruby/lib/eu3_classes/papal_information.rb
@@ -3,10 +3,10 @@ class EU3_Papal_Information
   def initialize(titles, papal_allies) # Title info, and allies of pope hash
     puts "Creating EU3 papacy section"
     @cardinals = Hash.new
-    create_cardinals(papal_allies)
+    add__eligible_cardinals(papal_allies)
   end
 
-  def create_cardinals(papal_allies)
+  def add__eligible_cardinals(papal_allies)
     # Convert to array of 2-member arrays (key->val becomes [key, val], etc.)
     # and then sort arrays by index 1 (the relations w/ pope), highest FIRST
       papal_allies = papal_allies.sort { |a, b| b[1]<=>a[1] }
@@ -16,6 +16,12 @@ class EU3_Papal_Information
       highest_relation = papal_allies[1]
     # Then convert it back to a hash, this time sorted highest relation first
       papal_allies = Hash[*papal_allies]
+    # Now add to cardinals as appropriate
+    papal_allies.each do |key, val|
+      if @cardinals.length < 15 || val == highest_relation
+        @cardinals[key] = val
+      end
+    end
   end
 
 end
