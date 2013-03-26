133d059a2dbaecb10c56e93871c938c149a28aa6
MiltonStanley <milton.stanley@gmail.com>
2013-03-26 15:15:40 -0500

    Pulls out highest relation held by anyone

--- Ruby/lib/eu3_classes/papal_information.rb
+++ Ruby/lib/eu3_classes/papal_information.rb
@@ -1,13 +1,19 @@
 class EU3_Papal_Information
 
-  def initialize(titles, papal_relations) # Title info, and allies of pope hash
+  def initialize(titles, papal_allies) # Title info, and allies of pope hash
     puts "Creating EU3 papacy section"
     @cardinals = Hash.new
-    create_cardinals(papal_relations)
+    create_cardinals(papal_allies)
   end
 
   def create_cardinals(papal_allies)
-    papal_allies = papal_allies.sort { |a, b| a[1]<=>b[1] } # Converts to arary
+    # Convert to array of 2-member arrays (key->val becomes [key, val], etc.)
+    # and then sort arrays by index 1 (the relations w/ pope)
+      papal_allies = papal_allies.sort { |a, b| a[1]<=>b[1] }
+    # Convert to 1-dimensions ([[val, val],...] becomes [val, val, ...])
+      papal_allies.flatten! 
+    highest_relation = papal_allies.last  # What is the best relation?
+    papal_allies = Hash[*papal_allies.flatten]
   end
 
 end
