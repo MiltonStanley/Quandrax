95ec094e04756d42b065967e50098ecedb0c8d3c
MiltonStanley <milton.stanley@gmail.com>
2013-03-29 13:06:47 -0500

    More work on eu3 prov tool

--- Ruby/tools/province_maker.rb
+++ Ruby/tools/province_maker.rb
@@ -1,8 +1,40 @@
-temp_file = File.open('../lib/templates/province.tmp')
-prov_stream = String.new
+class EU3_Province
+  attr_accessor :id
 
-while line = temp_file.read
-  prov_stream << line
+  def initialize(id)
+    @id = id
+  end
+
+  def add(line)
+
+  end
+
+  def write(location)
+
+  end
+
 end
 
-puts prov_stream.length
+def split_key_val_pair(line)
+  line.split('=',2)
+end
+
+def make_provinces(array)
+  temp_file = File.open('../lib/templates/province.tmp')
+  while line = temp_file.gets
+    key, value = split_key_val_pair(line)
+    if is_province_header?(key)
+      array << EU3_Province.new(key) 
+    else
+      array.last.add(line)
+    end
+  end
+end
+
+def is_province_header?(key)
+  key =~ /^\d+/
+end
+
+array = Array.new
+make_provinces(array)
+array.each { |prov| prov.write(Kernel) }
