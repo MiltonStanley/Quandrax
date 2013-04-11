4b8a58adf5f720ca80489755183555a84e6b2d41
MiltonStanley <milton.stanley@gmail.com>
2013-04-11 12:24:56 -0500

    Added test_hre_titles

--- Ruby/tests/tc_eu3_imperial_info.rb
+++ Ruby/tests/tc_eu3_imperial_info.rb
@@ -23,4 +23,12 @@ class TC_EU3_Imperial_Info < Test::Unit::TestCase
     assert_equal '1316', $EU3_IMPERIAL_INFO.hre_id
   end
 
+  def test_hre_titles
+    expected = %w[e_hre c_nassau b_nassau d_franconia
+                  c_leiningen b_leiningen c_wurzburg
+                  b_schweinfurt c_bamberg b_babenberg
+              ]
+    assert_equal expected, $EU3_IMPERIAL_INFO.hre_titles
+  end
+
 end
