8e26c0ecf3d62d87eb60497aa68352f23727f625
MiltonStanley <milton.stanley@gmail.com>
2013-04-12 15:21:10 -0500

    Added @dynasty accessor

--- Ruby/lib/ck2_classes/characters.rb
+++ Ruby/lib/ck2_classes/characters.rb
@@ -53,7 +53,7 @@ end
 class A_Character
   attr_accessor :id, :relations_to_pope, :friend_of_pope, 
   :birth_name, :employer, :job_title, :alive, 
-  :papal_relation_value
+  :papal_relation_value, :dynasty
 
   def initialize(line, pope_id)
     @id, _ = line.strip.split("=",2)
