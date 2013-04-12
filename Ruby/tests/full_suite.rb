e1e5e1c9987ca18637e5a411a3793f826c6bb98e
MiltonStanley <milton.stanley@gmail.com>
2013-04-12 15:39:59 -0500

    Added full test suite

---
+++ Ruby/tests/full_suite.rb
@@ -0,0 +1,11 @@
+require "test/unit"
+
+if __FILE__ == $0
+  Dir.chdir("..")
+  require './tests/setup_for_eu3_tests'
+end
+
+require './tests/ck2_test_suite'
+require './tests/eu3_test_suite'
+require './tests/tc_parsing'
+require './tests/tc_array_inversion'
