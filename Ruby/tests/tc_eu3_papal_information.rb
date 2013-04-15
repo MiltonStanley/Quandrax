57fca71cda3a9e6ff9352ca1c894e2e90b48c880
MiltonStanley <milton.stanley@gmail.com>
2013-04-15 18:18:01 -0500

    Tests for EU3 papal info

---
+++ Ruby/tests/tc_eu3_papal_information.rb
@@ -0,0 +1,22 @@
+##############################################
+#
+# This assumes WilliamBeginning.ck2 testfile
+#
+# This Global Object: $EU3_PAPAL_INFORMATION
+#
+##############################################
+
+require "test/unit"
+if __FILE__ == $0
+  Dir.chdir("..")
+  require './tests/setup_for_eu3_tests'
+end
+
+class TC_PapalInformation < Test::Unit::TestCase
+
+  def test_creation
+    assert $EU3_PAPAL_INFORMATION.instance_of?(EU3_Papal_Information),
+          'Failed to create EU3 Papal information'
+  end
+
+end
