9e7e04e60ed1bce68c68cbaeadd5c5560a1f3c5f
MiltonStanley <milton.stanley@gmail.com>
2013-04-15 18:32:09 -0500

    Better name for papal info object

--- Ruby/lib/file_handling.rb
+++ Ruby/lib/file_handling.rb
@@ -75,9 +75,9 @@ def write_file(new_file)
   $EU3_IMPERIAL_INFO.write(new_file)
   $EU3_DEFENDERS_OF_THE_FAITH = EU3_Defenders_of_the_Faith.new
   $EU3_DEFENDERS_OF_THE_FAITH.write(new_file)
-  $EU3_PAPAL_INFO = EU3_Papal_Information.new($CK2_TITLES.titles, 
+  $EU3_PAPAL_INFORMATION = EU3_Papal_Information.new($CK2_TITLES.titles, 
                                               $CK2_CHARACTERS.papal_relations)
-  $EU3_PAPAL_INFO.write(new_file)
+  $EU3_PAPAL_INFORMATION.write(new_file)
   print_from_temp(new_file, './lib/templates/papacy.tmp')
   print_from_temp(new_file, './lib/templates/trade.tmp')  # This is fine
   #$EU3_PROVINCES = EU3_Provinces.new($CK2_PROVINCES.provinces, $CK2_TITLES.titles)
