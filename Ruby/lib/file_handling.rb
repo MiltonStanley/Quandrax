ce76ed445dc770d10349fec2fced280fa2f02d03
MiltonStanley <milton.stanley@gmail.com>
2013-04-16 17:57:08 -0500

    Passing in whole objects now

--- Ruby/lib/file_handling.rb
+++ Ruby/lib/file_handling.rb
@@ -75,8 +75,7 @@ def write_file(new_file)
   $EU3_IMPERIAL_INFO.write(new_file)
   $EU3_DEFENDERS_OF_THE_FAITH = EU3_Defenders_of_the_Faith.new
   $EU3_DEFENDERS_OF_THE_FAITH.write(new_file)
-  $EU3_PAPAL_INFORMATION = EU3_Papal_Information.new($CK2_TITLES.titles, 
-                                              $CK2_CHARACTERS.papal_relations)
+  $EU3_PAPAL_INFORMATION = EU3_Papal_Information.new($CK2_TITLES, $CK2_CHARACTERS)
   $EU3_PAPAL_INFORMATION.write(new_file)
   print_from_temp(new_file, './lib/templates/papacy.tmp')
   print_from_temp(new_file, './lib/templates/trade.tmp')  # This is fine
