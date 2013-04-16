5cd9852524e234c7567a0e36800076615157c80a
MiltonStanley <milton.stanley@gmail.com>
2013-04-16 15:28:04 -0500

    Write 100 times: I will not comment out code

--- Ruby/lib/eu3_classes/papal_information.rb
+++ Ruby/lib/eu3_classes/papal_information.rb
@@ -2,99 +2,14 @@ class EU3_Papal_Information
 
   def initialize(titles, papal_allies) # Title info, and allies of pope hash
     puts "Creating EU3 papacy section"
-    #@cardinals = create_cardinals(titles, papal_allies)
+    @cardinals = create_cardinals(titles, papal_allies)
   end
 
   def write(location)
-    #location.puts "papacy=\n{\n\tcontroller=\"#{@cardinals[0]}\""
-    #location.puts "\tcrusade_target=\"---\"\n\tcrusade_start=\"1.1.1\""
-    #location.puts "\tlast_excom=\"1.1.1\"\n\tpapacy_active=yes"
-    #write_cardinals(@cardinals, location)
-    #location.puts "}"
-  end
-=begin
-  def write_cardinals(cardinals, location)
-    cardinal_names = ["Stephenos", "Jacobos", "Philippos", "Mattathaios", "Andreas",
-                      "Markos", "Petros", "Paulos", "Judas Thaddaeos", "Bartholomaios",
-                      "Thomas Didymus", "Loukas", "Antipas", "Ignatius", "Polycarpos"
-                     ]
-    cardinal_id = 0
-    cardinals.each do |tag| 
-    location.puts "\tcardinal=\n\t{\n\t\tname=\"#{cardinal_names[cardinal_id]}\""
-    location.puts "\t\tlocation=132\n\t\tcontroller=\"#{tag}\"\n\t\tid="
-    cardinal_id += 1
-    location.puts "\t\t{\n\t\t\tid=#{cardinal_id}\n\t\t\ttype=42\n\t\t}\n\t}"  
-    end
   end
 
   def create_cardinals(titles, papal_allies)
     cardinals = papal_allies
-    cardinals = convert_ids_to_titles(cardinals, titles)
-    cardinals = convert_titles_to_tags(cardinals, $TM_CK2_EU3)
-    cardinals = sort_cardinals(cardinals)
-    average_relation = get_average_relation(cardinals)
-    cardinals = highest_relations(cardinals, average_relation)
     cardinals
   end
-
-  def sort_cardinals(cardinals)
-    # Convert to array of 2-member arrays (key->val becomes [key, val], etc.)
-    #   and then sort arrays by index 1 (the relations w/ pope), highest FIRST
-      _temp_hash = cardinals.sort { |a, b| b[1]<=>a[1] }
-    # Convert to 1-dimensions ([[val, val],...] becomes [val, val, ...])
-      _temp_hash.flatten!
-    # Then convert it back to a hash, this time sorted highest relation first
-      _temp_hash = Hash[*_temp_hash]
-    # Now add to cardinals as appropriate
-    _temp_hash
-  end
-
-  def convert_ids_to_titles(cardinals, titles)
-    _temp_hash = Hash.new   # Can't change cardinals in block
-    cardinals.each do |key, val|  #  ID 1
-      titles.each do |title, a_title|    
-        if a_title.holder_id == key
-          if a_title.name =~ /^b_/
-            _temp_hash[a_title.liege] = val
-          else
-            _temp_hash[a_title.name] = val
-            break
-          end
-        end
-      end
-    end
-    _temp_hash
-  end
-
-  def convert_titles_to_tags(cardinals, tag_map)
-    _temp_hash = Hash.new
-    cardinals.each do |key, val|
-      tag = tag_map[key]
-      if _temp_hash[tag].nil?
-        _temp_hash[tag] = val
-      else
-        _temp_hash[tag] = val unless _temp_hash[tag] > val
-      end
-    end
-    _temp_hash
-  end
-
-  def get_average_relation(hash)
-    total_relations = 0
-    hash.each_value { |val| total_relations += val}
-    average_relation = total_relations / hash.length
-    average_relation
-  end
-
-  def highest_relations(hash, average_relation)
-    _temp_array = Array.new
-    hash.each do |tag, relations|
-      break if _temp_array.length >= 15
-      number_of_entries = relations / average_relation
-      1..number_of_entries.times { _temp_array << tag } if number_of_entries > 1
-      _temp_array << tag if number_of_entries <= 1
-    end
-    _temp_array    
-  end
-=end
 end
