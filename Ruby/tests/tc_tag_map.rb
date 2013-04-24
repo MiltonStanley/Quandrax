bd4a5180113046826e0f5a4d8d6eaebdda4b62cf
MiltonStanley <milton.stanley@gmail.com>
2013-04-24 11:13:34 -0500

    Added test_no_nils

--- Ruby/tests/tc_tag_map.rb
+++ Ruby/tests/tc_tag_map.rb
@@ -6,4 +6,9 @@ end
 
 class TestTagMap < Test::Unit::TestCase
 
+  def test_no_nils
+    $TM_CK2_EU3.each { |title, tag| assert !tag.nil?, "Tag expected for #{
+                                                      title} but was nil." }
+  end
+
 end
