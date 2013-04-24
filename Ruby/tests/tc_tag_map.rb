3bcd641ce665ff616eb99e2983e5f7a50550c1d3
MiltonStanley <milton.stanley@gmail.com>
2013-04-24 11:54:03 -0500

    Added test_tag_length

--- Ruby/tests/tc_tag_map.rb
+++ Ruby/tests/tc_tag_map.rb
@@ -1,7 +1,7 @@
 require "test/unit"
 if __FILE__ == $0
   Dir.chdir("..")
-  require './lib/maps/tag_map'
+  require './tests/setup_for_ck2_tests'
 end
 
 class TestTagMap < Test::Unit::TestCase
@@ -9,6 +9,11 @@ class TestTagMap < Test::Unit::TestCase
   def test_no_nils
     $TM_CK2_EU3.each { |title, tag| assert !tag.nil?, "Tag expected for #{
                                                       title} but was nil." }
+  end
+
+  def test_tag_length
+    $TM_CK2_EU3.each { |title, tag| assert_equal 3, tag.length, 
+                        "#{title}'s tag length error" }
   end
 
 end
