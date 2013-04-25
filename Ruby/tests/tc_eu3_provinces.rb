b5e3cfc19777b91d4490d592c50a5d07ecc623af
MiltonStanley <milton.stanley@gmail.com>
2013-04-25 12:40:51 -0500

    Added EU3_Provinces test

---
+++ Ruby/tests/tc_eu3_provinces.rb
@@ -0,0 +1,17 @@
+##############################################
+#
+# This assumes WilliamBeginning.ck2 testfile
+#
+# This Global Object: $EU3_PROVINCES
+#
+##############################################
+
+require "test/unit"
+if __FILE__ == $0
+  Dir.chdir("..")
+  require './tests/setup_for_eu3_tests'
+end
+
+class TestEU3Provinces < Test::Unit::TestCase
+
+end
