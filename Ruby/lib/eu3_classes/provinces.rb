49d68f045ae66fb411f734caf41dd3c07e75b608
MiltonStanley <milton.stanley@gmail.com>
2013-04-26 15:38:30 -0500

    Updated update_from_ck2 in EU3_Provinces class

--- Ruby/lib/eu3_classes/provinces.rb
+++ Ruby/lib/eu3_classes/provinces.rb
@@ -8,7 +8,7 @@ class EU3_Provinces
   def initialize(provinces, titles)
     @provinces = load_provinces_from_template # Array[eu3_id] = A_Province
     @province_map = invert($PM_CK2_EU3) 
-    @provinces = update_from_ck2(@provinces)
+    @provinces = update_from_ck2
   end
 
   def write(location)
@@ -20,6 +20,7 @@ class EU3_Provinces
 
   def update_from_ck2
     provinces = @provinces
+    provinces
   end
 
   def load_provinces_from_template
