21c18108c1b380f69c0761cf5307aa33a497084f
MiltonStanley <milton.stanley@gmail.com>
2013-04-30 21:19:20 -0500

    Added test_basic_mappings

--- Ruby/tests/tc_culture_map.rb
+++ Ruby/tests/tc_culture_map.rb
@@ -6,4 +6,11 @@ end
 
 class Test_Culture_Map < Test::Unit::TestCase
 
+  def test_basic_mappings
+    expecteds = { 'norman' => 'normand', 'occitan' => 'occitain' }
+    expecteds.each do |ck2, eu3|
+      assert_equal eu3, $CM_CK2_EU3[ck2]
+    end
+  end
+
 end
