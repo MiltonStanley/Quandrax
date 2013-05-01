fc4efce1e1143fb55fd29fa589fce030c782f92b
MiltonStanley <milton.stanley@gmail.com>
2013-04-30 21:55:38 -0500

    Skips provinces that have no culture (wastelands)

--- Ruby/tests/tc_culture_map.rb
+++ Ruby/tests/tc_culture_map.rb
@@ -9,7 +9,7 @@ class Test_Culture_Map < Test::Unit::TestCase
 
   def test_valid_keys
     $CK2_PROVINCES.provinces.each do |a_province|
-      next if a_province.nil?
+      next if a_province.nil? || a_province.culture.nil?
       assert $CM_CK2_EU3.has_key?(a_province.culture),
         "Failed to find #{a_province.culture} key in culture map!"
     end
