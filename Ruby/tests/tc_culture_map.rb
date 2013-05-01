70cde2abd9838b2f85f0508de9d34ece574de946
MiltonStanley <milton.stanley@gmail.com>
2013-04-30 21:43:00 -0500

    test_basic_mappings replaced by test_valid_keys

--- Ruby/tests/tc_culture_map.rb
+++ Ruby/tests/tc_culture_map.rb
@@ -2,14 +2,16 @@ require "test/unit"
 if __FILE__ == $0
   Dir.chdir("..")
   require './lib/maps/culture_map'
+  require './tests/setup_for_eu3_tests'
 end
 
 class Test_Culture_Map < Test::Unit::TestCase
 
-  def test_basic_mappings
-    expecteds = { 'norman' => 'normand', 'occitan' => 'occitain' }
-    expecteds.each do |ck2, eu3|
-      assert_equal eu3, $CM_CK2_EU3[ck2]
+  def test_valid_keys
+    $CK2_PROVINCES.provinces.each do |a_province|
+      next if a_province.nil?
+      assert $CM_CK2_EU3.has_key?(a_province.culture),
+        "Failed to find #{a_province.culture} key in culture map!"
     end
   end
 
