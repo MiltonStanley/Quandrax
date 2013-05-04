85acd9262fcd47b456cf5254814a5b2740a01e45
MiltonStanley <milton.stanley@gmail.com>
2013-05-04 18:54:32 -0500

    Processing history

--- tools/nation_maker.rb
+++ tools/nation_maker.rb
@@ -1,9 +1,36 @@
-file = File.open('../lib/templates/nations.tmp', 'r')
+file = File.open('./sweden.txt', 'r')
 
 class EU3Nation
 
   def initialize(tag)
     @tag = tag
+    @in_history = true
+    @history = String.new
+  end
+
+  def add(line)
+    @in_history = false if is_flags?(line)
+    process_history(line) if @in_history
+  end
+
+  def write
+    puts "#{@tag}=\n{\n\t#{@history}"
+  end
+
+  def process_history(line)
+    @history << line
+  end
+
+  def is_history?(line)
+    line =~ /^\t(history=)/
+  end
+
+  def is_flags?(line)
+    line =~ /^\t(flags=)/
+  end
+
+  def is_capital?(line)
+    line =~ /^\t(capital)/
   end
 
 end
@@ -13,8 +40,18 @@ def is_new_nation?(line)
 end
 
 nations = Hash.new
+current_nation = String.new
 
 file.each_line do |line|
-  tag = line.chop.chomp if is_new_nation?(line)
-  nations[tag] = EU3Nation.new(tag)
+  if is_new_nation?(line)
+    tag = line.chomp.chop
+    current_nation = tag
+    nations[current_nation] = EU3Nation.new(tag)
+  else
+    nations[current_nation].add(line)
+  end
 end
+
+a_nation = nations['SWE']
+
+a_nation.write
