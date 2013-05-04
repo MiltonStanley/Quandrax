fc4e230f1e1b15d7dba094be93629572119a6d2d
MiltonStanley <milton.stanley@gmail.com>
2013-05-04 17:46:19 -0500

    Made EU3Nation class, stored in nations hash

--- tools/nation_maker.rb
+++ tools/nation_maker.rb
@@ -1,7 +1,20 @@
-Dir.chdir("..")
+file = File.open('../lib/templates/nations.tmp', 'r')
 
-file = File.open('./lib/templates/nations.tmp', 'r')
+class EU3Nation
+
+  def initialize(tag)
+    @tag = tag
+  end
+
+end
+
+def is_new_nation?(line)
+  line =~ /^\w{3}=/
+end
+
+nations = Hash.new
 
 file.each_line do |line|
-  puts line if line =~ /^\w{3}=/
+  tag = line.chop.chomp if is_new_nation?(line)
+  nations[tag] = EU3Nation.new(tag)
 end
