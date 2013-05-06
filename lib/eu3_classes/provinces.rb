3650d1d67cbf4457bcd3465be0dc15a9828cdbe2
MiltonStanley <milton.stanley@gmail.com>
2013-05-06 14:05:30 -0500

    Added discovery_religion_dates accessor

--- lib/eu3_classes/provinces.rb
+++ lib/eu3_classes/provinces.rb
@@ -69,7 +69,7 @@ end
 
 class An_EU3_Province
   attr_accessor :id, :name, :owner, :controller, :cores, :culture, :religion,
-    :discovery_dates, :patrol
+    :discovery_dates, :patrol, :discovery_religion_dates
 
   def initialize(id)
     @controller, @culture, @religion, @capital, @owner, @citysize, @garrison, 
