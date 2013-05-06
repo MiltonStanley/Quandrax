9f49a09183477ef19033ab68a4141a142a8d6c57
MiltonStanley <milton.stanley@gmail.com>
2013-05-06 12:56:57 -0500

    Added test_discovery_dates

--- tests/tc_eu3_provinces.rb
+++ tests/tc_eu3_provinces.rb
@@ -93,6 +93,18 @@ class TestEU3Provinces < Test::Unit::TestCase
     expected_religions.each do |id, religion|
       assert_equal religion, $EU3_PROVINCES.provinces[id].religion
     end
+    $EU3_PROVINCES.provinces[82].write(Kernel)
+  end
+
+  def test_discovery_dates
+    expecteds = {
+        5  => %w[9999.1.1 9999.1.1 1.1.1 1.1.1 1.1.1 1.1.1 9999.1.1 9999.1.1 9999.1.1 9999.1.1],
+        82 => %w[9999.1.1 9999.1.1 1.1.1 1.1.1 1.1.1 1.1.1 9999.1.1 9999.1.1 9999.1.1 9999.1.1],
+      1423 => %w[9999.1.1 9999.1.1 9999.1.1 9999.1.1 9999.1.1 9999.1.1 9999.1.1 9999.1.1 9999.1.1 9999.1.1]
+    }
+    expecteds.each do |id, dates|
+      assert_equal dates, $EU3_PROVINCES.provinces[id].discovery_dates
+    end
   end
 
 end
