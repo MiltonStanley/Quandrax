3ea6aed60395de04c24392bde8d5cbe404e1ff1e
MiltonStanley <milton.stanley@gmail.com>
2013-05-05 21:25:10 -0500

    Deleted province template writing

--- lib/file_handling.rb
+++ lib/file_handling.rb
@@ -80,7 +80,6 @@ def write_file(new_file)
   print_from_temp(new_file, './lib/templates/trade.tmp')  # This is fine
   $EU3_PROVINCES = EU3_Provinces.new($CK2_PROVINCES.provinces, $CK2_TITLES.titles, $EU3_HEADER.tag)
   $EU3_PROVINCES.write(new_file)
-  print_from_temp(new_file, './lib/templates/province.tmp')
   print_from_temp(new_file, './lib/templates/nations.tmp')
   print_from_temp(new_file, './lib/templates/diplomacy.tmp')
   print_from_temp(new_file, './lib/templates/combat.tmp')
