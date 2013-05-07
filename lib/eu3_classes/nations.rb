e9d8d266676002b3f41bcc945a1b548544aea22d
MiltonStanley <milton.stanley@gmail.com>
2013-05-07 13:57:15 -0500

    Skip the nil province

--- lib/eu3_classes/nations.rb
+++ lib/eu3_classes/nations.rb
@@ -8,6 +8,7 @@ class EU3_Nations
   def get_nations(ck2_provinces)
     nations = Array.new
     ck2_provinces.each do |a_prov|
+      next if a_prov.nil?
       tag = $TM_CK2_EU3[a_prov.title]
       nations << tag unless nations.include? tag
     end
