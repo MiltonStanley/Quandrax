b325662d451c372fe82adefe629ecdaf5a4e9a2f
MiltonStanley <milton.stanley@gmail.com>
2013-05-07 13:37:43 -0500

    Added nations Array - holds all nations required

--- lib/eu3_classes/nations.rb
+++ lib/eu3_classes/nations.rb
@@ -1,7 +1,17 @@
 class EU3_Nations
+  attr_accessor :nations
 
-  def initialize
+  def initialize(ck2_provinces)
+    @nations = get_nations(ck2_provinces)
+  end
 
+  def get_nations(ck2_provinces)
+    nations = Array.new
+    ck2_provinces.each do |a_prov|
+      tag = $TM_CK2_EU3[a_prov.title]
+      nations << tag unless nations.include? tag
+    end
+    nations
   end
 
 end
