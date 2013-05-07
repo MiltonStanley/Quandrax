550b2d24fefb40572ddcfd657702a9c8de1ce070
MiltonStanley <milton.stanley@gmail.com>
2013-05-07 13:04:43 -0500

    Created TestEU3Nations

---
+++ tests/tc_eu3_nations.rb
@@ -0,0 +1,22 @@
+##############################################
+#
+# This assumes WilliamBeginning.ck2 testfile
+#
+# This Global Object: $EU3_NATIONS
+#
+##############################################
+
+require "test/unit"
+if __FILE__ == $0
+  Dir.chdir("..")
+  require './tests/setup_for_eu3_tests'
+end
+
+class TestEU3Nations < Test::Unit::TestCase
+  
+  def test_creation
+    assert $EU3_NATIONS.instance_of?(EU3_Nations),
+          'Failed to create EU3 Province information'
+  end
+
+end
